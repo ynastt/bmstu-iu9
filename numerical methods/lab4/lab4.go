7835199da79c8f17ab68982a049085b09dc37119
Anastasia Yarovikova <86785664+ynastt@users.noreply.github.com>
2023-03-26 14:08:51 +0300

    Create lab4.go

---
+++ numerical methods/lab4/lab4.go
@@ -0,0 +1,88 @@
+// индивидуальный вариант 29
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func direct(b, a, c, d []float64, size int) (alpha, beta []float64) {
+	alpha = append(alpha, -c[0] / b[0])
+	beta = append(beta, d[0] / b[0])
+	var y float64
+	for i := 1; i < size - 1; i++ {
+		y = a[i - 1] * alpha[i - 1] + b[i]
+		alpha = append(alpha, -c[i] / y)
+		beta = append(beta, (d[i] - a[i - 1] * beta[i - 1]) / y)
+	}
+	y = a[size - 2] * alpha[size - 2] + b[size - 1]
+	beta = append(beta, (d[size - 1] - a[size - 2] * beta[size - 2]) / y)
+	return alpha, beta
+}
+
+func reverse(alpha, beta []float64, size int) (x []float64) {
+	x = make([]float64, size)
+	x[size - 1] = beta[size - 1]
+	for i := size - 2; i >= 0; i-- {
+		x[i] = alpha[i] * x[i + 1] + beta[i]
+	}
+	return x
+}
+
+func f(x float64) float64 {
+	return 3 * math.Sin(2 * x) 
+}
+
+//через вольфрам альфа 
+func analyticalSolution(x float64) float64 {
+	return (2 - 0.75 * x) * math.Cos(2 * x) + 1.5 * math.Sin(x) * math.Cos(x)  	
+}
+
+var (
+	p = 0.0
+	q = 4.0
+	a = analyticalSolution(0)
+	b = analyticalSolution(1)
+)
+
+func main() {
+	n := 9
+	h := 1.0 / float64(n+1)
+	xs := make([]float64, 0, n+1)
+	for i := 0; i <= n; i++ {
+		xs = append(xs, float64(i) * h)
+	}
+	as := make([]float64, 0, n-1)
+	bs := make([]float64, 0, n)
+	cs := make([]float64, 0, n-1)
+	ds := make([]float64, 0, n)
+
+	for i := 0; i < n; i++ {
+		as = append(as, 1 - h / 2 *p)
+		cs = append(cs, 1 + h / 2 * p)
+	}
+
+	for i := 0; i < n + 1; i++ {
+		bs = append(bs, h * h * q - 2)
+		ds = append(ds, h * h * f(float64(i) * h))
+	}
+
+	ds[0] = h * h * f(0) - a * (1 - h / 2 * p)
+	ds[len(ds) - 1] = h * h * f(float64(len(ds) - 1) * h) - b * (1 + h / 2 * p)
+
+	alpha, beta := direct(bs, as, cs, ds, n + 1)
+	ys := reverse(alpha, beta, n + 1)
+
+	//maxDiff := 0.0
+
+	for i, y := range ys {
+		fmt.Printf("x=%.16f, y=%.16f, y*=%.16f  |y-y*|=%.16f\n",
+					xs[i], analyticalSolution(xs[i]), y, math.Abs(y-analyticalSolution(xs[i])))
+
+		// if math.Abs(y-analyticalSolution(xs[i])) > maxDiff {
+		// 	maxDiff = math.Abs(y - analyticalSolution(xs[i]))
+		// }
+	}
+
+	//fmt.Printf("max_diff=%.16f\n", maxDiff)
+}
