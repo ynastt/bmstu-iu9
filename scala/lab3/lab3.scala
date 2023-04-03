80d695d3937acd824048a553ee8b1380459e08ce
Anastasia Yarovikova <86785664+ynastt@users.noreply.github.com>
2023-04-04 01:02:24 +0300

    Update lab3.scala

--- scala/lab3/lab3.scala
+++ scala/lab3/lab3.scala
@@ -1,1 +1,56 @@
 //aaaaaaaaaaa
+// flag: 1 - for variable's name, 2 - for constants type T, 3 +, 4 -, 5 *, 6 /
+class Formula[T](a: T, flag: Int) {
+  val formula = a
+
+  this(str: String, f: Int) = Formula(str, 1)
+  this(value: T) = Formula(value, 2)
+
+  def solve[S]()(implicit ops: EquationSystemOps[T, S]): Option[(S, S)] = {
+    val d = num.minus(num.times(a11, a22), num.times(a21, a12))
+    if (num.equiv(d, num.zero)) {
+      None
+    } else {
+      val d1 = num.minus(num.times(b1, a22), num.times(a12, b2))
+      val d2 = num.minus(num.times(a11, b2), num.times(b1, a21))
+      Some((ops.div(d1, d), ops.div(d2, d)))
+    }
+  }
+}
+
+trait FormulaOps[T] {
+  def add(a: T, b: T): T
+}
+
+object FormulaOps {
+  implicit def float_ops[T](implicit
+      frac: Fractional[T]
+  ): FormulaOps[T, T] =
+    new FormulaOps[T, T] {
+      def div(a: T, b: T): T = frac.div(a, b)
+      def add(a: T, b: T): T = frac.plus(a, b)
+    }
+
+  implicit def int_ops[T](implicit
+      integral: Integral[T]
+  ): FormulaOps[T, Ratio[T]] =
+    new FormulaOps[T, Ratio[T]] {
+      def div(a: T, b: T): Ratio[T] = new Ratio(a, b)
+
+    }
+}
+
+object Main extends App {
+  val f1 = new Formula("")
+  val s2 = new EquationSystem(((3, 4), (2, 5)), (18, 19))
+  val s3 = new EquationSystem(((2.0, 1.0), (3.0, 2.0)), (5.0, 3.0))
+  val s4 = new EquationSystem(((2, 1), (4, 2)), (5, 3))
+  println(s1.solve())
+  println(s2.solve())
+  println(s3.solve())
+  println(s4.solve())
+
+  val s5 = new EquationSystem((("a", "b"), ("c", "d")), ("e", "f"))
+  /* Следующая строчка не компилируется */
+  // println(s5.solve())
+}
