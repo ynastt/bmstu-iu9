e26af71c3459fccb270ed2491d9540d93cb08aeb
Anastasia Yarovikova <86785664+ynastt@users.noreply.github.com>
2023-06-09 20:16:02 +0300

    Alexandr Vladimirovich, please, accept my lab TT

--- compilers/lab33/main.py
+++ compilers/lab33/main.py
@@ -40,14 +40,13 @@ class UnknownVar(SemanticError):
         return f'Необъявленная переменная {self.varname}'
 
 class NotOOPInheritance(SemanticError):
-    def __init__(self, pos, name, ancestor):
+    def __init__(self, pos, name):
         self.pos = pos
         self.varname = name
-        self.ancestor = ancestor
 
     @property
     def message(self):
-        return f'Наследование {self.varname} от {self.ancestor} не является ООП'
+        return f'Нарушение Объектно-Ориентированного наследования: {self.varname}'
 
 
 class NotSharedAncestor(SemanticError):
@@ -161,12 +160,30 @@ class TypeDef:
     name_coord : pe.Position
     ancestorType : Type
     ancestor_coord : pe.Position
-    varsDefs: VarsDef  
+    varsDefs: VarsDef
+    possibleFields: dict
     @pe.ExAction
     def create(attrs, coords, res_coords):
         name, ancestorType_, varsDefs = attrs
         cname, cravn, crecord_kw, canctype, cvardefs, cend_kw, csemicolon = coords
-        return TypeDef(name, cname, ancestorType_, canctype ,varsDefs)
+        return TypeDef(name, cname, ancestorType_, canctype ,varsDefs, dict())
+    
+    def findPossibleFields(self, types):
+        fields = {}
+        ownfields = types[self.name].varsDefs
+        for f in ownfields:
+            for n in f.names:
+                fields[n] = f.type
+        anc = types[self.name].ancestorType
+        while type(anc) is UserType:
+            ofs = types[anc.name].varsDefs
+            for f in ofs:
+                for n in f.names:
+                    fields[n] = f.type
+            anc = types[anc.name].ancestorType    
+        # print(f'all possible fields: {fields}')
+        return fields  
+
 
 
 class Statement(abc.ABC):
@@ -188,16 +205,24 @@ class Program:
                     raise RepeatedVariable(vardef.names_coord, name)
                 else:
                     vars[name] = vardef.type   
-        print(f'vars: {vars}\n')
+        print(f'\nvars:')
+        for v in vars: print(v, vars[v])
+
         types = {}     
         for typedef in self.type_defs:
                 name = typedef.name
                 if name in types:
                     raise RepeatedType(typedef.name_coord, name)
                 else:
-                    types[name] = typedef.ancestorType
-        print(f'types: {types}\n')
+                    types[name] = typedef
+        for typedef in self.type_defs:
+            name = typedef.name
+            types[name].possibleFields = types[name].findPossibleFields(types)   
+        print(f'\n\ntypes:')
+        for t in types: print(t, types[t].ancestorType, types[t].varsDefs, types[t].possibleFields )
+        
         for statement in self.statements:
+            print(f'\n\n***current statement: {statement}')
             statement.check(vars, types)
 
 class Expr(abc.ABC):
@@ -220,18 +245,19 @@ class AssignStatement(Statement):
     def check(self, vars, types):
         if type(self.variable) is SingleVarExpr:
             print("single")
+            print(self.variable)
             v = self.variable.varname[0]
-            print(f' varname: {v}, expr: {self.expr.varname[0]}')
+            print(f' varname: {v},\n expr: {self.expr}')
             if v not in vars:
-                raise UnknownVar(self.variable, self.var_coord)
+                raise UnknownVar(self.var_coord, v)
             
             # print("variable is known")
             self.expr.check(vars, types)
-
+            is_numeric = lambda t: t in (BasicType.Integer, BasicType.Real)
             if vars[v] == self.expr.type:
-                print(f'here are  varname: {v}, expr: {self.expr.varname[0]}')
+                print(f'  same type')
                 return
-            if vars[v] == BasicType.Real and self.expr.type == BasicType.Integer:
+            if is_numeric(vars[v]) and is_numeric(self.expr.type):
                 return
             # Можно присваивать одну структуру другой структуре, если они имеют общего предка (прямо или косвенно).
             if type(vars[v]) is UserType and type(self.expr.type) is UserType:
@@ -239,20 +265,20 @@ class AssignStatement(Statement):
                 lftype = vars[v].name
                 # print(lftype)
                 lf.add(lftype)
-                anc = types[lftype]
+                anc = types[lftype].ancestorType
                 while type(anc) is UserType:
                     lf.add(anc.name) 
-                    anc = types[anc.name]
+                    anc = types[anc.name].ancestorType
                 # print(f'all lf ancs: {lf}')
 
                 rf = set()
                 rftype = self.expr.type.name
                 # print(rftype)
                 rf.add(rftype)
-                anc = types[rftype]
+                anc = types[rftype].ancestorType
                 while type(anc) is UserType:
                     rf.add(anc.name)
-                    anc = types[anc.name]
+                    anc = types[anc.name].ancestorType
                 # print(f'all rf ancs: {rf}')
                 shared = lf & rf
                 # print(f'shared ancestor {shared}')
@@ -263,66 +289,52 @@ class AssignStatement(Statement):
                 
             # Можно присваивать указатель на производную структуру указателю на базовую структуру
             if type(vars[v]) is PointerType and type(self.expr.type) is PointerType:
-                #  ОК, когда: указать на базовую структуру := указатель на производную структуру
-                # print(vars[v], vars[v].pointerTo, vars[v].pointerTo.name)                
-                # print(self.expr.type)
+                #  ОК, когда: указатель на базовую структуру := указатель на производную структуру
                 rftype = self.expr.type.pointerTo.name
-                anc = types[rftype]
+                anc = types[rftype].ancestorType
                 # print(anc)
                 if type(anc) is not EmptyAncestorType:
                     if vars[v].pointerTo.name != anc.name:
                         raise PointerBadAssignment(self.var_coord, self.variable.varname[0], self.expr.varname[0])
                     else :
-                        # print("ok")
                         return
                 raise BinBadType(self.var_coord, vars[v], ':=', self.expr.type)    
             else:           
-                raise BinBadType(self.var_coord, vars[v], ':=', self.expr.type)
-        if type(self.variable) is DerefenceExpr:  # этого не было в тз
-            # ОК, когда: pr^ := r; 
-            # pr : POINTER TO Rectangle; r : Rectangle;
-            print("DEREFENCE")
-            v = self.variable.varname
-            print(f' varname: {v}, expr: {self.expr}')
-            vtype = vars[v.varname[0]].pointerTo.name
-            print(vtype)
-            extype = vars[self.expr.varname[0]].name                
-            print(extype)
-            if vtype != extype:
-                #  Если 
-                # проверим, есть ли общий предок, если нет - ошибка 
-                
-                lf = set()
-                lf.add(vtype)
-                print()
-                anc = types[vtype]
-                while type(anc) is UserType:
-                    lf.add(anc.name) 
-                    anc = types[anc.name]
-                print(f'all lf ancs: {lf}')
-                
-                rf = set()
-                rf.add(extype)
-                anc = types[extype]
-                while type(anc) is UserType:
-                    rf.add(anc.name)
-                    anc = types[anc.name]
-                print(f'all rf ancs: {rf}')
-                shared = lf & rf
-                print(f'shared ancestor {shared}')
-                if len(shared) == 0:
-                    raise NotSharedAncestor(self.var_coord, v.varname[0], self.expr.varname[0])
-                else:
-                    print("ok")
-                    return
-            else :
-                print("ok")
-                return    
-        else:
+                raise BinBadType(self.var_coord, vars[v], ':=', self.expr.type)   
+        elif type(self.variable) is DerefenceExpr:
+            print("derefence var")
+            v = self.variable
+            ex = self.expr
+            # print(f' varname: {v}, expr: {ex}')
+            self.variable.check(vars, types)
+            self.expr.check(vars, types)
+        elif type(self.variable) is VarWithFieldsExpr:
             print("fields var")
-            # v = self.variable
-            # print(f' varname: {v}, expr: {self.expr}')
-        
+            v = self.variable
+            ex = self.expr
+            print(f' varname: {v},\n expr: {ex}')
+            self.variable.check(vars, types)
+            self.expr.check(vars, types)
+            # проверяю, соответствие типов присваимовой пееременной ex prи поля var.field переменной var, которому присваивается значение
+            print("...LATER...")
+            common_type = None
+            is_numeric = lambda t: t in (BasicType.Integer, BasicType.Real)
+
+            if self.variable.type == self.expr.type:
+                return
+            elif is_numeric(self.variable.type) and is_numeric(self.expr.type):
+                return
+            elif type(self.variable.type) is PointerType and type(self.expr.type) is PointerType:
+                rtype = self.expr.type.pointerTo.name
+                anc = types[rtype].ancestorType
+                if type(anc) is not EmptyAncestorType:
+                    if self.variable.type.pointerTo.name != anc.name:
+                        raise PointerBadAssignment(self.var_coord, self.variable, self.expr)
+                    else :
+                        return
+                raise BinBadType(self.var_coord, self.variable.type, ':=', self.expr.type)
+            else: 
+                raise BinBadType(self.var_coord, self.variable.type, ':=', self.expr.type)
 
 
 @dataclass
@@ -345,10 +357,13 @@ class IfStatement(Statement):
         return IfStatement(cond, ccond, then_branch, else_branch)
     def check(self, vars, types):
         self.condition.check(vars, types)
+        print(f'IF CONDITION: {self.condition}')
         if self.condition.type != BasicType.Boolean:
             raise NotBoolCond(self.cond_coord, self.condition.type)
-        self.then_branch.check(vars, types)
-        self.else_branch.check(vars, types)
+        for t in self.then_branch:
+            t.check(vars, types)
+        for e in self.else_branch:
+            e.check(vars, types)
 
 
 @dataclass
@@ -363,9 +378,12 @@ class WhileStatement(Statement):
         return WhileStatement(cond, ccond, body)
     def check(self, vars, types):
         self.condition.check(vars, types)
+        print(f'WHILE CONDITION: {self.condition}')
         if self.condition.type != BasicType.Boolean:
             raise NotBoolCond(self.cond_coord, self.condition.type)
-        self.body.check(vars, types)
+        print("here")
+        for b in self.body:
+            b.check(vars, types)
 
 
 @dataclass
@@ -384,21 +402,13 @@ class ForStatement(Statement):
         return ForStatement(varname, cvar, start, cstart, end, cend, body)
     def check(self, vars, types):
         if self.variable not in vars:
-            raise UnknownVar(self.variable, self.var_coord)
+            raise UnknownVar(self.var_coord, self.variable)
         NotIntFor.check(vars[self.variable], self.var_coord)
         self.start.check(vars, types)
         NotIntFor.check(self.start.type, self.start_coord)
         self.end.check(vars, types)
         NotIntFor.check(self.end.type, self.end_coord)
         self.body.check(vars, types)
-
-
-@dataclass
-class StatementsBlock(Statement):
-    body : list[Statement]
-    def check(self, vars, types):
-        for statement in self.body:
-            statement.check(vars, types)
 
 
 @dataclass
@@ -409,9 +419,26 @@ class EmptyStatement(Statement):
 
 @dataclass
 class DerefenceExpr(Expr):
-    varname : Any 
+    varname : Any
+    var_coord : pe.Position
+
+    @pe.ExAction
+    def create(attrs, coords, res_coord):
+        varname = attrs
+        cvarname, cder = coords
+        return DerefenceExpr(varname, cvarname)  
+       
     def check(self, vars, types):
-        pass
+        dname = self.varname[0]
+        # print(f' deref varname: {dname}')
+        if type(dname) is SingleVarExpr:
+            v = dname.varname[0]
+            # print(f'  name: {v}')
+            try:
+                self.type = vars[v]
+            except KeyError:
+                raise UnknownVar(self.var_coord, v)
+            
 
 
 @dataclass
@@ -426,9 +453,8 @@ class SingleVarExpr(Expr):
         return SingleVarExpr(varname, cvarname)
     
     def check(self, vars, types):
-        # print("he")
         v = self.varname[0]
-        # print(f'varname: {v}, {type(v)}')
+        print(f'  single varname: {v}, type: {vars[v]}')
         try:
             self.type = vars[v]
         except KeyError:
@@ -438,15 +464,61 @@ class SingleVarExpr(Expr):
 @dataclass
 class VarWithFieldsExpr(Expr):
     var: list[str]
+    field: str
     var_coord : pe.Position
-    def create(varlist):
-        @pe.ExAction
-        def action(attrs, coords, res_coords):
-            cvar, cdot, cfield = coords
-            return VarWithFieldsExpr(varlist, cvar.start)
-        return action
+    @pe.ExAction
+    def create(attrs, coords, res_coord):
+        var, field = attrs
+        cvar, cdot, cfield = coords
+        return VarWithFieldsExpr(var, field, cvar.start)
     def check(self, vars, types):
-        pass
+        varname = self
+        varfield = self.field
+        print(f'\nvar of var: {varname}\n  field: {varfield}')
+        print("are fields correct?")
+        # OOP?
+        fields = []
+        while type(varname) is VarWithFieldsExpr:
+            print(f'   var: {varname}\n   field: {varfield}')
+            fields.append(varfield)
+
+            if type(varname.var) is SingleVarExpr:
+                name = varname.var.varname[0]
+                try:
+                    typ = vars[name]
+                except KeyError:
+                    raise UnknownVar(self.var_coord, name)
+                
+                print(f'    name: {name}, field: {varfield}')
+                fields =list(reversed(fields))
+                print(fields)
+                if type(vars[name]) is UserType:
+                    typ =vars[name].name
+                elif type(vars[name]) is PointerType:
+                    typ =vars[name].pointerTo.name    
+                print(typ)
+                fds = types[typ].possibleFields
+                prev = name
+                for f in fields:
+                    print(f'pos fields: {fds}')
+                    if len(fds) != 0 and f not in fds:
+                        raise NotOOPInheritance(self.var_coord, prev + "." + f)
+                    t = fds[f]
+                    try:
+                        self.type = fds[f]
+                    except KeyError:
+                        raise UnknownVar(self.var_coord, f)
+                    if type(t) is UserType: 
+                        fds = types[t.name].possibleFields
+                    if type(t) is PointerType: 
+                        fds = types[t.pointerTo.name].possibleFields   
+                    prev = f
+                    print(fds)
+                    print(self.type)
+                break
+            varname = varname.var
+            varfield = varname.field
+
 
 
 @dataclass
@@ -572,7 +644,7 @@ NVarsDefs |= NVarsDefs, NVarsDef, lambda vds, vd: vds + [vd]
 NVarsDef |= NVars, ':', NType, ';' , VarsDef.create
 
 NVars |= VARNAME, lambda v: [v]
-NVars |= VARNAME, ',', NVars, lambda v, vs: vs + [v]
+NVars |= VARNAME, ',', NVars, lambda v, vs:  [v] + vs
 
 NType |= KW_INTEGER, lambda: BasicType.Integer
 NType |= KW_REAL, lambda: BasicType.Real
@@ -592,7 +664,6 @@ NStatement |= KW_WHILE, NExpr, KW_DO, NStatements, KW_END, WhileStatement.create
 NStatement |= (
     KW_FOR, NVar, ':=', NExpr, KW_TO, NExpr, KW_DO, NStatement, KW_END, ForStatement.create
 )
-NStatement |= KW_BEGIN, NStatements, KW_END, StatementsBlock
 NStatement |= EmptyStatement
 
 
@@ -628,17 +699,9 @@ NFactor |= NVar
 NFactor |= NConst
 NFactor |= '(', NExpr, ')'
 
-# NVar |= NVar, '^', DerefenceExpr
-# NVar |= VARNAME, lambda var: VarWithFields([var])
-# NVar |= NVar, '.', VARNAME, lambda v, f: VarWithFields([v, f])
-
-NVar |= NVar, '^', DerefenceExpr
+NVar |= NVar, '^', DerefenceExpr.create
 NVar |= VARNAME, SingleVarExpr.create
-NVar |= NVar, '.', VARNAME, lambda v, f: VarWithFieldsExpr.create([v, f])
-# NSingleVar |= VARNAME, SingleVarExpr.create
-# NVarWithFields |= NVar, '.', VARNAME, lambda v, f: VarWithFieldsExpr.create([v, f])
-
-
+NVar |= NVar, '.', VARNAME, VarWithFieldsExpr.create
 
 NConst |= INTEGER, lambda v: ConstExpr(v, BasicType.Integer)
 NConst |= REAL, lambda v: ConstExpr(v, BasicType.Real)
@@ -662,8 +725,8 @@ for filename in sys.argv[1:]:
             tree = parser.parse(f.read())
             # pprint(tree)
             tree.check()
-            print('Программа корректна')
+            print('Correct program')
     except pe.Error as e:
-        print(f'Ошибка {e.pos}: {e.message}')
+        print(f'Error {e.pos}: {e.message}')
     except Exception as e:
         print(e)
